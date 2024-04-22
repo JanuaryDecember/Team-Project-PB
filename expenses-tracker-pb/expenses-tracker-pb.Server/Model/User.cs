be80fcd6e759923e3e1620bcde3f503c99865f82
DaV <DaV@DESKTOP-GH660AU>
2024-04-22 14:51:01 +0200

    New fields for authentication with email

--- expenses-tracker-pb/expenses-tracker-pb.Server/Model/User.cs
+++ expenses-tracker-pb/expenses-tracker-pb.Server/Model/User.cs
@@ -5,6 +5,13 @@ public class User : IdentityUser
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public ICollection<Wallet> Wallets { get; set; }
+
+    // Field for authentication throw email
+    public bool? EmailTwoFactorAuthenticationEnabled {  get; set; }
+    public string? EmailTwoFactorAuthenticationCode { get; set; }
+    public DateTime? EmailTwoFactorAuthenticationExpiryTime { get; set; }
+    public DateTime? LastEmailTwoFactorAuthenticationCodeSent { get; set; }
+
     public string? GoogleAuthKey { get; set; }
     public string? ResetPasswordCode { get; set; }
     public DateTime? ResetPasswordCodeExpireTime { get; set; }
