c4632c91d0915b2199949f9f746174aa8b6e6c87
DaV <DaV@DESKTOP-GH660AU>
2024-04-24 00:51:50 +0200

    #

--- expenses-tracker-pb/expenses-tracker-pb.Server/Program.cs
+++ expenses-tracker-pb/expenses-tracker-pb.Server/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ETDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ETDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DawidConnection")));
 
 builder.Services.AddCors();
 
