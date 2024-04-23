5267dbc3ad0abaef1abf79579ea4ca2116a7915b
kasiagrygo <131908455+kasiagrygo@users.noreply.github.com>
2024-04-23 17:25:13 +0200

    opcja dodania zdjecia profilowego uzytkownika

--- expenses-tracker-pb/expenses-tracker-pb.Server/Model/User.cs
+++ expenses-tracker-pb/expenses-tracker-pb.Server/Model/User.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class User : IdentityUser
 {
@@ -9,4 +10,7 @@ public class User : IdentityUser
     public string? ResetPasswordCode { get; set; }
     public DateTime? ResetPasswordCodeExpireTime { get; set; }
     public ICollection<Category>? UserCategories { get; set; }
+
+    [Column(TypeName = "varbinary(max)")]
+    public byte[]? ProfilePicture { get; set; }
 }
