453053e04144595269a0f50f990a48fb720b29e2
Aikoocioq <ola.didluch@o2.pl>
2024-05-06 15:38:54 +0200

    Baza

--- expenses-tracker-pb/expenses-tracker-pb.Server/Program.cs
+++ expenses-tracker-pb/expenses-tracker-pb.Server/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ETDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DawidConnection")));
+builder.Services.AddDbContext<ETDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Ola2Connection")));
 
 builder.Services.AddCors();
 
