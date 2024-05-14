d64c74fb416d674cd8836981ed6f609ce04bb631
edobrzycka <edobrzycka2803@gmail.com>
2024-05-14 09:05:23 +0200

    Sprint14

--- expenses-tracker-pb/expenses-tracker-pb.Server/Model/User.cs
+++ expenses-tracker-pb/expenses-tracker-pb.Server/Model/User.cs
@@ -24,8 +24,8 @@ public class User : IdentityUser
     public SecurityQuestion? SecurityQuestion { get; set; }
     public string? SecurityQuestionAnswer { get; set; }
 
-    //[Column(TypeName = "varbinary(max)")]
+    [Column(TypeName = "varbinary(max)")]
     //w postgersql nie ma typu varbinary(max) zamiast tego jest bytea, na potrzeby testów zamieniłem ten kod na bytea
-    [Column(TypeName = "bytea")]
+   // [Column(TypeName = "bytea")]
     public byte[]? ProfilePicture { get; set; }
 }
--- expenses-tracker-pb/expenses-tracker-pb.Server/Program.cs
+++ expenses-tracker-pb/expenses-tracker-pb.Server/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ETDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("JanConnection")));
+builder.Services.AddDbContext<ETDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EwaConnection")));
 
 builder.Services.AddCors();
 
