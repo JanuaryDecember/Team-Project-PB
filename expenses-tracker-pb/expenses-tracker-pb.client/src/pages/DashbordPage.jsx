7145b5b46dc11ef9afa8e0c2350195132373db1b
emek-k <emakorycki@gmail.com>
2024-05-26 17:29:34 +0200

    PZTRAC-39 - Dodanie kafelka z budzetem do dashboard.

--- expenses-tracker-pb/expenses-tracker-pb.client/src/pages/DashbordPage.jsx
+++ expenses-tracker-pb/expenses-tracker-pb.client/src/pages/DashbordPage.jsx
@@ -53,52 +53,61 @@ const DashboardPage = (props) => {
                     <span className="fs-4"><strong>{translation[props.language].Dashboard.Greetings + user.username}!</strong></span>
                     <p className="mt-2"><em>{translation[props.language].Dashboard.Desc}</em></p>
                     <div className="row mt-5" style={{ gap: "0px" }}>
-                        <Link to="/wallet" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                        <Link to="/wallet" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my w-100 m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.flaticon.com/512/493/493389.png' alt="Wallet Icon" />
                                 <span>{translation[props.language].Dashboard.Wallets}</span>
                             </div>
                         </Link>
-                        <Link to="/import" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                        <Link to="/import" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.flaticon.com/128/4013/4013427.png' alt="Import Icon" />
                                 <span>{translation[props.language].Dashboard.Import}</span>
                             </div>
                         </Link>
-                        <Link to="/export" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                        <Link to="/export" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.flaticon.com/128/5859/5859742.png' alt="Export Icon" />
                                 <span>{translation[props.language].Dashboard.Export}</span>
                             </div>
                         </Link>
-                        <Link to="/myReceipt" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                    </div>
+
+                    <div className="row mt-5" style={{ gap: "0px" }}>
+                        <Link to="/myReceipt" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.flaticon.com/128/217/217905.png' alt="Receipt Icon" />
                                 <span>{translation[props.language].Dashboard.Receipts}</span>
                             </div>
                         </Link>
-                    </div>
-
-                    <div className="row mt-5" style={{ gap: "0px" }}>
-                        <Link to="/account/settings/twoFactorAuthentication" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                        <Link to="/account/settings/twoFactorAuthentication" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my w-100 m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.freepik.com/256/4448/4448933.png' alt="TwoFactor Icon" />
                                 <span>{translation[props.language].Dashboard.TwoFactor}</span>
                             </div>
                         </Link>
-                        <Link to="/report" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                        <Link to="/report" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.freepik.com/256/2912/2912773.png' alt="Reports Icon" />
                                 <span>{translation[props.language].Dashboard.Report}</span>
                             </div>
                         </Link>
-                        <Link to="/categories" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                    </div>
+
+                    <div className="row mt-5" style={{ gap: "0px" }}>
+                        <Link to="/budget" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
+                            <div className="card background-my m-auto text-center">
+                                <img className="icons" src='https://cdn-icons-png.freepik.com/512/11370/11370113.png' alt="Budget Icon" />
+                                <span>{translation[props.language].Dashboard.Budget}</span>
+                            </div>
+                        </Link>
+                        <Link to="/categories" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.freepik.com/256/9309/9309662.png' alt="Categories Icon" />
                                 <span>{translation[props.language].Dashboard.Categories}</span>
                             </div>
                         </Link>
-                        <Link to="/profile" className="link col-sm-3" style={{ minWidth: "25%", margin: "0 0 5px 0" }}>
+                        <Link to="/profile" className="link col-sm-3" style={{ minWidth: "33%", margin: "0 0 5px 0" }}>
                             <div className="card background-my m-auto text-center">
                                 <img className="icons" src='https://cdn-icons-png.freepik.com/256/1077/1077063.png' alt="Profile Icon" />
                                 <span>{translation[props.language].Dashboard.Profile}</span>
